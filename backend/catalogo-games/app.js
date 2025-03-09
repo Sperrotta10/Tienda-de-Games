fca9a87aabebb190405c946437a4d6b0a5829679
Sperrotta10 <santiagoperrotta10@gmail.com>
2025-03-09 11:11:39 -0400

    modificaciones en apps.js (se establecio la ruta y cambios en la conexion a base de datos y servidor)

--- backend/catalogo-games/app.js
+++ backend/catalogo-games/app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const routes = require('./src/router/games')
 
+// Cargar el archivo .env específico del microservicio
 dotenv.config()
 
 const PORT = process.env.PORT;  // definimos el puerto del servidor
@@ -11,17 +13,16 @@ const app = express()
 app.use(express.json())
 
 //routes
-app.use('/api/catalogo-games')
+app.use('/api/catalogo-games', routes())
 
 // conexion a base de datos y servidor
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    }).then(() => {
-        console.log("✅ Servicio de catalogo de juegos Conectado a mongodb")
-        app.listen(PORT, () => {
-            console,log(`Servidor esta corriendo en el puesto ${PORT}`)
-        })
-    }).catch((err) => {
-        console.error("🚫 Error al conectarse a la base de datos -> Servicio de catalogo", err)
-    })
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("✅ Servicio de catalogo de juegos Conectado a mongodb");
+    app.listen(PORT, () => {
+      console.log(`Servidor esta corriendo en el puerto http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("🚫 Error al conectarse a la base de datos -> Servicio de catalogo", err);
+  });
