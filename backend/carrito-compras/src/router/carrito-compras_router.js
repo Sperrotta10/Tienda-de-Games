07d3c06aa9739650d0bef416e8842c611069ef04
JesusAraujoDEV <j2a0a0a5@gmail.com>
2025-03-09 18:43:21 -0400

    Implementación de la configuración y el pool de conexiones de PostgreSQL; refactorización del enrutador carrito-compras.

--- backend/carrito-compras/src/router/carrito-compras_router.js
+++ backend/carrito-compras/src/router/carrito-compras_router.js
@@ -1,15 +1,13 @@
 const express = require('express');
-//const FrutasService = require('./../services/frutas_service');
-//const validatorHandler = require('./../middlewares/validator_handler');
-//const { createFrutaSchema, updateFrutaSchema, getFrutaSchema } = require('./../schemas/frutas_schema');
 
-//const service = new FrutasService();
+//const CarritoService = require('../services/order.service');
+const validatorHandler = require('../middlewares/validator.handler');
+const {
+  getCarritoSchema,
+  createCarritoSchema,
+  addItemSchema,
+} = require('../schemas/order.schema');
 
-const router = express.Router();
 
-
-router.get('/', (req, res) => {
-  res.send('hola mundo');
-});
 
 module.exports = router;
