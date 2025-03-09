1378eecfb8d0bdfafcc796aba32418f9ffa4987f
Sperrotta10 <santiagoperrotta10@gmail.com>
2025-03-09 16:23:34 -0400

    Modificaciones en los schemas (cambio de atributos, salida json)

--- backend/gestion-pedidos/src/schema/pedidos.js
+++ backend/gestion-pedidos/src/schema/pedidos.js
@@ -1,12 +1,14 @@
 const mongoose = require('mongoose');
 
 // Esquema para los ítems del pedido (juegos comprados)
-const itemPedidoSchema = new mongoose.Schema({
+const itemPedidoSchema = new mongoose.Schema(
+    {
     juegoId: { type: mongoose.Schema.Types.ObjectId, ref: 'Juego', required: true },
     titulo: { type: String, required: true },
     precio: { type: Number, required: true },
-    cantidad: { type: Number, required: true, min: 1 },
-  });
+  },
+  {_id: false}
+);
   
 // Esquema principal para el pedido
 const pedidoSchema = new mongoose.Schema({
@@ -21,6 +23,25 @@ const pedidoSchema = new mongoose.Schema({
     },
     fechaCreacion: { type: Date, default: Date.now },
     fechaActualizacion: { type: Date, default: Date.now },
+});
+
+// Personalizar la salida JSON
+pedidoSchema.set('toJSON', {
+    transform: function (doc, ret) {
+      return {
+        _id: ret._id,
+        usuarioId: ret.usuarioId,
+        items: ret.items.map(item => ({
+          juegoId: item.juegoId,
+          titulo: item.titulo,
+          precio: item.precio,
+        })),
+        precioTotal: ret.precioTotal,
+        estado: ret.estado,
+        fechaCreacion: ret.fechaCreacion,
+        fechaActualizacion: ret.fechaActualizacion,
+      };
+    },
 });
 
 const Pedido = mongoose.model('Pedido', pedidoSchema);
