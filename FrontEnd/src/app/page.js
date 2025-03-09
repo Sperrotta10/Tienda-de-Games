9733887a4ddbd96b6aa69d9303500d48de33dfc6
Maanuuuu <manueldavidnava31@gmail.com>
2025-03-09 18:32:47 -0400

    Change default page

--- FrontEnd/src/app/page.js
+++ FrontEnd/src/app/page.js
@@ -1,9 +1,13 @@
-import Image from "next/image";
-
+import estilos from "../Styles/library.module.css";
+import GameCard from "../Components/GameCard.js";
 export default function Home() {
   return (
-    <div >
-      <h1>Home</h1>
+    <div style={{ display: 'flex', gap: '20px', flexWrap: 'wrap' }}>
+      <GameCard
+        gameName="Fifa 25"
+        backgroundImage="fifa25.avif"
+      />
+      
     </div>
   );
 }
