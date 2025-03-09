474234633a53d17f5a1003c9b73ce402ee4a3b7d
Maanuuuu <manueldavidnava31@gmail.com>
2025-03-09 14:30:23 -0400

    Agregando Carpeta de BackEnd

---
+++ BackEnd/catalogo-games/app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const routes = require('./src/router/games')
+
+// Cargar el archivo .env específico del microservicio
+dotenv.config()
+
+const PORT = process.env.PORT;  // definimos el puerto del servidor
+
+// levantamos el servidor
+const app = express()
+app.use(express.json())
+
+//routes
+app.use('/api/catalogo-games', routes())
+
+// conexion a base de datos y servidor
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("✅ Servicio de catalogo de juegos Conectado a mongodb");
+    app.listen(PORT, () => {
+      console.log(`Servidor esta corriendo en el puerto http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("🚫 Error al conectarse a la base de datos -> Servicio de catalogo", err);
+  });
---
+++ BackEnd/catalogo-games/src/router/games.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const {create_game, delete_game, delete_gameName, delete_AllGame, update_game, obtener_game, obtener_gameID, obtener_gameName} = require('../controller/games')
+
+// TEMPORAL
+const {validationGames} = require('../validator/games') // temporal
+const {Game} = require('../schema/games') // temporal
+const fs = require('fs');
+const path = require('path');
+
+// Cargar el archivo JSON (TEMPORAL)
+const gamesData = JSON.parse(
+    fs.readFileSync(path.join(__dirname, 'catalogo.json'), 'utf-8')
+);
+
+const routes = () => {
+
+    // creamos el enrutador
+    const routes = express.Router();
+
+    // ruta temporal solo para agregar todo el catalogo de juegos
+    routes.post('/createAllGames', async (req, res) => {
+
+        try {
+            // Validar los datos con Zod
+            const validatedGames = gamesData.map(game => {
+                const result = validationGames(game);
+                if (!result.success) {
+                    throw new Error(`Datos no válidos: ${JSON.stringify(result.error)}`);
+                }
+                return result.data; // Devuelve solo los datos validados
+            });
+    
+            // Insertar los juegos en la base de datos
+            const result = await Game.insertMany(validatedGames);
+            res.status(201).json({message: "Juegos agregados correctamente"})
+        } catch (error) {
+            console.error('Error al insertar los juegos:', error);
+            res.status(500).json({message: "Error al insertar juegos", error})
+        }
+    })
+
+    //api para crear juegos ✅
+    routes.post('/create-game', create_game)
+
+    // api para eliminar juegos por su id ✅
+    routes.delete('/delete-game/:id_game', delete_game)
+
+    // api para eliminar juegos por su nombre ✅
+    routes.delete('/delete-Namegame/:nameGame', delete_gameName)
+
+    // api para eliminar juegos por su nombre ✅
+    routes.delete('/delete-allGame', delete_AllGame)
+
+    // api para actualizar juegos ✅
+    routes.put('/update-game/:id_game', update_game)
+
+    // api para obtener juegos ✅
+    routes.get('/get-games', obtener_game)
+
+    // api para obtener juegos por id ✅
+    routes.get('/get-game/:id_game', obtener_gameID)
+
+    // api para obtener juegos por nombre ✅
+    routes.get('/get-Namegame/:nameGame', obtener_gameName)
+
+    return routes;
+}
+
+module.exports = routes;
