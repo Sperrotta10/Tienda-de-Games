8e1a942b83ce66ffa61a20da2379f005bbb69d8b
Sperrotta10 <santiagoperrotta10@gmail.com>
2025-03-09 12:15:17 -0400

    modificando la tabla de los requisitos para que no se genere un id, y modificando como se genera el json de catalogo-games

--- backend/catalogo-games/src/schema/games.js
+++ backend/catalogo-games/src/schema/games.js
@@ -1,12 +1,15 @@
 const mongoose = require('mongoose');
 
-const requisitosSistemaSchema = new mongoose.Schema({
+const requisitosSistemaSchema = new mongoose.Schema(
+    {
     sistema_operativo: { type: String, required: true },
     procesador: { type: String, required: true },
     memoria: { type: String, required: true },
     graficos: { type: String, required: true },
     almacenamiento: { type: String, required: true },
-});
+    },
+    { _id: false }
+);
 
 const gameSchemas = new mongoose.Schema({
     titulo: { type: String, required: true },
@@ -28,6 +31,32 @@ const gameSchemas = new mongoose.Schema({
     enlace_trailer: { type: String, required: true},
     portada: { type: String, required: true}
 });
+
+gameSchemas.set('toJSON', {
+    transform: function (doc, ret) {
+      // Define el orden de los campos
+      return {
+        titulo: ret.titulo,
+        genero: ret.genero,
+        desarrollador: ret.desarrollador,
+        editor: ret.editor,
+        fecha_lanzamiento: ret.fecha_lanzamiento,
+        precio: ret.precio,
+        plataformas: ret.plataformas,
+        descripcion: ret.descripcion,
+        requisitos_sistema: {
+          minimos: ret.requisitos_sistema.minimos,
+          recomendados: ret.requisitos_sistema.recomendados,
+        },
+        idiomas_disponibles: ret.idiomas_disponibles,
+        clasificacion_edad: ret.clasificacion_edad,
+        multijugador: ret.multijugador,
+        cooperativo: ret.cooperativo,
+        enlace_trailer: ret.enlace_trailer,
+        portada: ret.portada,
+      };
+    },
+  });
 
 // creamos nuestro modelo y lo exportamos
 const Game = mongoose.model("Game", gameSchemas);
