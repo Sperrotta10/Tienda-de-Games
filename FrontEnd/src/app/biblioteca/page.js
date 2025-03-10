f9011f066076756a7b8f542e9b9cf277d7f30bd5
Maanuuuu <manueldavidnava31@gmail.com>
2025-03-09 21:04:02 -0400

    Add: Route to Biblioteca

---
+++ FrontEnd/src/app/biblioteca/page.js
@@ -0,0 +1,70 @@
+import estilos from "../../Styles/library.module.css";
+import GameCard from "../../Components/GameCard.js";
+import DropBox from "../../Components/Dropbox.js";
+
+export default function library() {
+
+    const handleSelect = (option) => {
+        console.log("Seleccionaste:", option);
+      };
+    return (
+        <div className={estilos.library_container}>
+            <h1 className={estilos.title}>Biblioteca</h1>
+            <div className={estilos.games_container}>
+                <GameCard
+                    gameName="Fifa 25"
+                    backgroundImage="fifa25.avif"
+                />
+                <GameCard
+                    gameName="Fifa 25"
+                    backgroundImage="fifa25.avif"
+                />
+                <GameCard
+                    gameName="Fifa 25"
+                    backgroundImage="fifa25.avif"
+                />
+                <GameCard
+                    gameName="Fifa 25"
+                    backgroundImage="fifa25.avif"
+                />
+                <GameCard
+                    gameName="Fifa 25"
+                    backgroundImage="fifa25.avif"
+                />
+                <GameCard
+                    gameName="Fifa 25"
+                    backgroundImage="fifa25.avif"
+                />
+                <GameCard
+                    gameName="Fifa 25"
+                    backgroundImage="fifa25.avif"
+                />
+                <GameCard
+                    gameName="Fifa 25"
+                    backgroundImage="fifa25.avif"
+                />
+                 <GameCard
+                    gameName="Fifa 25"
+                    backgroundImage="fifa25.avif"
+                />
+                 <GameCard
+                    gameName="Fifa 25"
+                    backgroundImage="fifa25.avif"
+                />
+                 <GameCard
+                    gameName="Fifa 25"
+                    backgroundImage="fifa25.avif"
+                />
+                 <GameCard
+                    gameName="Fifa 25"
+                    backgroundImage="fifa25.avif"
+                />
+            </div>
+            
+            <div className={estilos.dropbox_container}>
+            <h2>Ordenar por: </h2>
+            <DropBox options={["Recientemente", "Orden Alfábetico", "Fecha de Lanzamiento", "Fecha de Adquisición"]}  />
+             </div>
+        </div>
+    );
+}
