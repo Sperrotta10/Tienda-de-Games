86d6d0ecbdaf0dfe0dfac10f62d53fc22bea963e
Maanuuuu <manueldavidnava31@gmail.com>
2025-03-09 20:59:48 -0400

    Change: GamerCard in Biblioteca

--- FrontEnd/src/Components/GameCard.js
+++ FrontEnd/src/Components/GameCard.js
@@ -4,21 +4,21 @@ import styles from '../Styles/library.module.css';
 const GameCard = ({ gameName, backgroundImage }) => {
     return (
         <div className={styles.cardContainer}>
-        <div className={styles.imageBackground}>
-          {/* Usamos el componente Image de Next.js */}
-          <Image
-            src={`/assets/images/${backgroundImage}`}
-            alt={gameName}
-            layout="fill" // Hace que la imagen ocupe todo el contenedor
-            objectFit="cover" // Asegura que la imagen cubra el área del contenedor
-            quality={75} // Calidad de la imagen
-            priority={false} // Lazy loading habilitado por defecto
-          />
-          <div className={styles.gradient}>
-            <h3 className={styles.gameName}>{gameName}</h3>
-          </div>
+            <div className={styles.imageBackground}>
+            {/* Usamos el componente Image de Next.js */}
+            <Image
+                src={`/assets/images/${backgroundImage}`}
+                alt={gameName}
+                fill={true} // Hace que la imagen ocupe todo el contenedor
+                loading='lazy' 
+                quality={100} // Calidad de la imagen
+                priority={false} // Lazy loading habilitado por defecto
+            />
+                <div className={styles.gradient}>
+                    <h3 className={styles.gameName}>{gameName}</h3>
+                </div>
+            </div>
         </div>
-      </div>
     );
   };
 export default GameCard;
