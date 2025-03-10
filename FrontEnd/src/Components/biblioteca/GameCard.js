a141bfec9f2c367cba442d9a0e613ad7f60b1dcb
Maanuuuu <manueldavidnava31@gmail.com>
2025-03-10 13:40:55 -0400

    feat: Add biblioteca folder to Components

---
+++ FrontEnd/src/Components/biblioteca/GameCard.js
@@ -0,0 +1,33 @@
+import Image from 'next/image';
+import styles from '../../Styles/library.module.css';
+
+const GameCard = ({ gameName, backgroundImage }) => {
+    return (
+        <div className={styles.cardContainer}>
+            <div className={styles.imageBackground}>
+            {/* Usamos el componente Image de Next.js */}
+            <Image
+                src={`/assets/images/${backgroundImage}`}
+                alt={gameName}
+                fill={true} // Hace que la imagen ocupe todo el contenedor
+                loading='lazy' 
+                quality={100} // Calidad de la imagen
+                priority={false} // Lazy loading habilitado por defecto
+            />
+                <div className={styles.gradient}>
+                    <div  className={styles.gamecard_footer}>
+                        <h3 className={styles.gameName}>{gameName}</h3>
+                        <Image
+                            src="/assets/icons/download.svg"
+                            alt="Play"
+                            width={18}
+                            height={18}
+                            className={styles.whiteIcon}
+                        ></Image>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+  };
+export default GameCard;
