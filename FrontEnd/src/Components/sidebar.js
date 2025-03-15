2da23a91f2c0bed7465f5b2a20d2b109e4934064
Maanuuuu <manueldavidnava31@gmail.com>
2025-03-14 23:20:13 -0400

    fix(sidebar): ensure icons remain visible by placing images inside links

--- FrontEnd/src/Components/sidebar.js
+++ FrontEnd/src/Components/sidebar.js
@@ -1,52 +1,57 @@
 import Link from "next/link";
-import styles from "../Styles/sidebar.module.css";
 import Image from "next/image";
+import styles from "../Styles/sidebar.module.css";
 
 export default function Sidebar() {
   return (
     <aside className={styles.sidebar}>
       <div className={styles.logo}>
-        <Image 
+        <Link href="/">
+          <Image 
             src="/assets/icons/search.svg"
-            alt="cart icon"
+            alt="Logo"
             width={40}
             height={40}
-            className={styles.icon} 
-          /> 
+            className={styles.icon}
+          />
+        </Link>
       </div>
+
       <ul className={styles.sidebarNav}>
         <li className={styles.navItem}>
-          <Image 
-            src="/assets/icons/cart.svg"
-            alt="cart icon"
-            width={24}
-            height={24}
-            className={styles.icon} 
-          /> 
-          <span>Juegos</span>
-        </li>
-        <li className={styles.navItem}>
-          <Image 
-            src="/assets/icons/cart.svg"
-            alt="cart icon"
-            width={24}
-            height={24}
-            className={styles.icon} 
-          /> 
-          <Link href="/biblioteca" className={styles.navLink}>
-            <span>Biblioteca</span>
+          <Link href="/" className={styles.navLink}>
+            <Image 
+              src="/assets/icons/cart.svg"
+              alt="Inicio"
+              width={24}
+              height={24}
+              className={styles.icon}
+            />
+            <span>Inicio</span>
           </Link>
         </li>
+        
         <li className={styles.navItem}>
           <Image 
             src="/assets/icons/cart.svg"
-            alt="cart icon"
-            width={24}
-            height={24}
-            className={styles.icon} 
-          />  
-          <Link href="/" className={styles.navLink}>
-            <span>Tienda</span>
+            alt="Juegos"
+            width={18}
+            height={18}
+            className={styles.icon}
+          />
+          <span>Juegos</span>
+        </li>
+
+        <li className={styles.navItem}>
+          <Link href="/biblioteca" className={styles.navLink}>
+            <Image 
+              src="/assets/icons/cart.svg"
+              alt="Biblioteca"
+              width={24}
+              height={24}
+              className={styles.icon}
+            />
+            <span>Biblioteca</span>
           </Link>
         </li>
       </ul>
